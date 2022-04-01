3855a949a1b75d42bb8927a1e18ea0809462b73b
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-01 20:25:58 +0300

    module-info.java dto module info added

--- dto/src/main/java/module-info.java
+++ dto/src/main/java/module-info.java
@@ -1,2 +1,11 @@
 module application.dto.main {
+    requires org.jetbrains.annotations;
+    requires lombok;
+    requires com.fasterxml.jackson.annotation;
+
+    exports com.mairwunnx.dto.request.products;
+
+    exports com.mairwunnx.dto.response.products;
+
+    opens com.mairwunnx.dto.response.products to com.fasterxml.jackson.databind;
 }

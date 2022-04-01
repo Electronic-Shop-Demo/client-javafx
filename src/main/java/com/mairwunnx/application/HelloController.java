d0efab40b46ea7196799187569fa02138f4c99cf
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-01 13:14:01 +0300

    Controller method changed

--- src/main/java/com/mairwunnx/application/HelloController.java
+++ src/main/java/com/mairwunnx/application/HelloController.java
@@ -5,9 +5,7 @@ import com.mairwunnx.application.router.controller.RouterController;
 import com.mairwunnx.application.router.types.RouterArg;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,6 +13,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements RouterController, Initializable {
+    private Router router;
+
     @FXML
     private Label welcomeText;
 
@@ -25,27 +25,21 @@ public class HelloController implements RouterController, Initializable {
 
     @FXML
     protected void onHelloButtonClick() {
-        if (router != null) {
-            router.navigate("hello-test", new RouterArg<>("router", router));
-        }
-        /*welcomeText.setText("Welcome to JavaFX Application!");*/
-    }
-
-    private Router router;
-
-    @Override
-    public <T> void onShow(@NotNull final Stage stage, @NotNull final Scene scene, @Nullable final RouterArg<T> arg) {
-        final Router ss = getArgumentFromOrNull(arg, "router");
-        router = ss;
-    }
-
-    @Override
-    public void onExit(@NotNull final Stage stage, @NotNull final Scene scene) {
-
+        router.navigate("hello-test");
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
+    }
+
+    @Override
+    public <T> void onShow(@NotNull final Router router, @Nullable final RouterArg<T> arg) {
+        this.router = router;
+    }
+
+    @Override
+    public void onExit(@NotNull final Router router) {
 
     }
 }

7a8917519f031f1313e547fac5590c68a4e94d4c
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-03 20:18:02 +0300

    ZygoteInit.java zygote init class changed

--- src/main/java/com/mairwunnx/application/ZygoteInit.java
+++ src/main/java/com/mairwunnx/application/ZygoteInit.java
@@ -1,23 +1,9 @@
 package com.mairwunnx.application;
 
-import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.SystemUtils;
+import com.mairwunnx.application.application.di.GuiceInjector;
 
-@Log4j2
 public final class ZygoteInit {
     public static void main(final String[] args) {
-        try {
-            log.info(
-                "Starting application on the Java Virtual Machine {} {}, on the OS {} {} {}",
-                SystemUtils.JAVA_VERSION,
-                SystemUtils.JAVA_VENDOR,
-                SystemUtils.OS_NAME,
-                SystemUtils.OS_ARCH,
-                SystemUtils.OS_VERSION
-            );
-            Application.launch(Application.class, args);
-        } catch (final RuntimeException ex) {
-            log.error("An error occurred while application running", ex);
-        }
+        GuiceInjector.ensureModules(args);
     }
 }

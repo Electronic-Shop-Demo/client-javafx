9ad7d4a3632b966e8d6497b2c4a94355abed4db7
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-03 20:17:30 +0300

    GuiceInjector.java guice injector created

---
+++ src/main/java/com/mairwunnx/application/application/di/GuiceInjector.java
@@ -0,0 +1,22 @@
+package com.mairwunnx.application.application.di;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.mairwunnx.application.PostInit;
+import com.mairwunnx.application.application.di.modules.AppModule;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+public final class GuiceInjector {
+    @NotNull
+    @Getter
+    @Setter(value = AccessLevel.PRIVATE)
+    private static Injector injector;
+
+    public static void ensureModules(final String[] args) {
+        setInjector(Guice.createInjector(new AppModule(args)));
+        getInjector().getInstance(PostInit.class).init();
+    }
+}

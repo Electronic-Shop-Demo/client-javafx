18b3aabe95483b25650d5a57f47b9b6ec4c24b26
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-03 20:19:16 +0300

    Application.java application class rewrite

---
+++ src/main/java/com/mairwunnx/application/application/Application.java
@@ -0,0 +1,52 @@
+package com.mairwunnx.application.application;
+
+import com.mairwunnx.application.application.router.Router;
+import javafx.stage.Stage;
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Log4j2
+public final class Application extends javafx.application.Application {
+    @Nullable
+    private Router router = null;
+
+    @Override
+    public void init() {
+        log.info("Initializing the JavaFX application");
+        initializeProperties();
+        initializeRouter();
+    }
+
+    @Override
+    public void start(final @NotNull Stage stage) {
+        if (router != null) {
+            router.ensureStage(stage);
+            router.navigate(ApplicationRouter.Routes.MAIN.toString());
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (router != null) {
+            router.shutdown();
+            router = null;
+        } else {
+            log.warn("Router shutdown call skipped, router is null");
+        }
+    }
+
+    private void initializeProperties() {
+        try {
+            System.setProperty("prism.lcdtext", "false");
+            System.setProperty("prism.text", "t2k");
+        } catch (final SecurityException ex) {
+            log.error("Security error, has no able to change system properties", ex);
+        }
+    }
+
+    private void initializeRouter() {
+        router = new ApplicationRouter().buildRouter();
+        log.info("Application router created");
+    }
+}

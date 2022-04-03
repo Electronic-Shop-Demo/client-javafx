09029b1d785dcf263a5e6d4cab3ee10db1cb56bd
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-03 20:14:31 +0300

    Size.java moved to other package

---
+++ src/main/java/com/mairwunnx/application/application/router/types/Size.java
@@ -0,0 +1,4 @@
+package com.mairwunnx.application.application.router.types;
+
+public record Size(int width, int height) {
+}
--- src/main/java/com/mairwunnx/application/router/types/Size.java
+++
@@ -1,4 +0,0 @@
-package com.mairwunnx.application.router.types;
-
-public record Size(int width, int height) {
-}

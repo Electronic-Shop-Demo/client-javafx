8b672061db145f8c2dbefe262f95ad63dbb3b23c
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-04 18:52:13 +0300

    JfxView.java JavaFX view interface added with commonly implementations for other views

---
+++ src/main/java/com/mairwunnx/application/application/contracts/JfxView.java
@@ -0,0 +1,27 @@
+package com.mairwunnx.application.application.contracts;
+
+import com.mairwunnx.application.application.views.TopBar;
+import javafx.fxml.FXMLLoader;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public sealed interface JfxView permits TopBar {
+    @NotNull String layoutPath();
+
+    default void expose() {
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(layoutPath()));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (final IOException e) {
+            onExposeFailure(e);
+        }
+    }
+
+    default void onExposeFailure(final IOException e) {
+        throw new RuntimeException(e);
+    }
+}

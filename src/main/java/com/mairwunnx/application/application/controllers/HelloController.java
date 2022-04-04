f27af5e55c148897efd33c27acbedd714e5965cb
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-04 18:51:30 +0300

    HelloController.java formatted

--- src/main/java/com/mairwunnx/application/application/controllers/HelloController.java
+++ src/main/java/com/mairwunnx/application/application/controllers/HelloController.java
@@ -43,7 +43,6 @@ public final class HelloController implements RouterController, Initializable {
 
     private final ObservableList<ProductResponse> observableList = FXCollections.observableArrayList();
 
-
     @FXML
     private void onBackButtonClick() {
         router.back();

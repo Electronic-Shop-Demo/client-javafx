319cfe5828a150a6be82665647f39c9555e1be89
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-05 20:43:49 +0300

    InteractionUtils.java interaction utils added, for commonizing basic interact events

---
+++ src/main/java/com/mairwunnx/application/application/compact/InteractionUtils.java
@@ -0,0 +1,30 @@
+package com.mairwunnx.application.application.compact;
+
+import javafx.scene.Node;
+import javafx.scene.input.MouseButton;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+public final class InteractionUtils {
+    public static <T extends Node> void setOnUserInteract(
+        @NotNull final T node,
+        @NotNull final Consumer<T> consumer
+    ) {
+        node.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                consumer.accept(node);
+            }
+        });
+
+        node.setOnKeyReleased(event -> {
+            switch (event.getCode()) {
+                case SPACE, ENTER -> consumer.accept(node);
+                default -> noop();
+            }
+        });
+    }
+
+    private static void noop() {
+    }
+}

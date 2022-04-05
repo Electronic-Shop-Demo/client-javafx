dad9e179b479925ac5ada3640de4dc2897d32ddb
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-05 20:43:25 +0300

    HomeController.java focus discarding implemented

--- src/main/java/com/mairwunnx/application/application/controllers/home/HomeController.java
+++ src/main/java/com/mairwunnx/application/application/controllers/home/HomeController.java
@@ -5,6 +5,7 @@ import com.mairwunnx.application.application.router.controller.RouterController;
 import com.mairwunnx.application.application.router.types.RouterArg;
 import com.mairwunnx.application.application.views.TopBar;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -16,9 +17,14 @@ public class HomeController implements RouterController {
     @FXML
     private TopBar topbar;
 
+    @FXML
+    private AnchorPane root;
+
     @Override
     public <T> void onShow(@NotNull final Router router, @Nullable final RouterArg<T> arg) {
         this.router = router;
+        root.requestFocus();
+        root.setOnMousePressed(e -> root.requestFocus());
         topbar.setOnBackRequested(() -> log.debug("top bar back button clicked"));
         topbar.setOnSearchChanged(log::debug);
     }

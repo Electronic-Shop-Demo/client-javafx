ce97bde6299e82d26d4288307b87d71ba57611e8
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-07 18:13:36 +0300

    HttpClientComponent.java TODO notation added

--- src/main/java/com/mairwunnx/application/application/components/HttpClientComponent.java
+++ src/main/java/com/mairwunnx/application/application/components/HttpClientComponent.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import java.net.http.HttpClient;
 import java.time.Duration;
 
+// TODO: Move in the services layer.
+
 public final class HttpClientComponent {
     @Getter
     private final HttpClient httpClient;

20601e2ac84a31d442997d28b5a8a5c6b80f7e45
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-07 18:15:46 +0300

    RouterMapperImpl.java suppressed unused warning, reason API

--- src/main/java/com/mairwunnx/application/application/router/impl/RouterMapperImpl.java
+++ src/main/java/com/mairwunnx/application/application/router/impl/RouterMapperImpl.java
@@ -12,6 +12,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 @ApiStatus.Internal
 public final class RouterMapperImpl implements RouterMapper {
     @NotNull

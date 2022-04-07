84408768f4231739ba7f0cc6569dd7e43a028cde
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-07 18:14:30 +0300

    RouterGraph.java suppressed unused warning, reason API

--- src/main/java/com/mairwunnx/application/application/router/graph/RouterGraph.java
+++ src/main/java/com/mairwunnx/application/application/router/graph/RouterGraph.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+@SuppressWarnings("unused")
 public interface RouterGraph {
     @Nullable <T> RouterGraphEntry<T> getLast();
 

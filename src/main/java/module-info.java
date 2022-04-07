9f2b3bec308946e1c3aca7e4288c7a4951c6327c
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-07 16:40:42 +0300

    module-info.java module info code improved

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,15 @@
 module com.mairwunnx.application {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+
     requires org.jetbrains.annotations;
     requires lombok;
 
-    requires application.dto.main;
     requires java.net.http;
+    requires javax.inject;
+    requires com.google.guice;
 
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.annotation;
@@ -17,20 +18,19 @@ module com.mairwunnx.application {
     requires org.apache.commons.lang3;
     requires org.apache.logging.log4j;
 
-    requires javax.inject;
-    requires com.google.guice;
+    requires application.dto.main;
 
     opens com.mairwunnx.application to javafx.fxml, com.google.guice;
+    opens com.mairwunnx.application.application to com.google.guice, javafx.fxml;
     opens com.mairwunnx.application.application.di.modules to com.google.guice;
+    opens com.mairwunnx.application.application.controllers to com.google.guice, javafx.fxml;
+    opens com.mairwunnx.application.application.controllers.home to com.google.guice, javafx.fxml;
+    opens com.mairwunnx.application.application.components to com.google.guice, javafx.fxml;
     opens com.mairwunnx.application.application.views to javafx.fxml;
 
-    exports com.mairwunnx.application.application.router;
     exports com.mairwunnx.application;
     exports com.mairwunnx.application.application;
-    opens com.mairwunnx.application.application to com.google.guice, javafx.fxml;
-    exports com.mairwunnx.application.application.controllers;
-    opens com.mairwunnx.application.application.controllers to com.google.guice, javafx.fxml;
     exports com.mairwunnx.application.application.components;
-    opens com.mairwunnx.application.application.components to com.google.guice, javafx.fxml;
-    opens com.mairwunnx.application.application.controllers.home to com.google.guice, javafx.fxml;
+    exports com.mairwunnx.application.application.controllers;
+    exports com.mairwunnx.application.application.router;
 }

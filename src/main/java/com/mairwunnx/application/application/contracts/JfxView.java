8b53fcf5d25568306c61f1f3d13fb220140de4e8
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-08 23:30:30 +0300

    JfxView.java contract documented

--- src/main/java/com/mairwunnx/application/application/contracts/JfxView.java
+++ src/main/java/com/mairwunnx/application/application/contracts/JfxView.java
@@ -7,9 +7,24 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
+/**
+ * Contract for all JavaFX custom views can be exposed.
+ */
 public sealed interface JfxView permits TopBar {
+    /**
+     * Simply supplier for receiving layout path for this view.
+     *
+     * @return layout path for this view.
+     */
     @NotNull String layoutPath();
 
+    /**
+     * Does expose (inflate) layout for this root. Also sets controller
+     * as instance to this.
+     *
+     * In case if {@link FXMLLoader} will throw exception that exception will
+     * delegate to {@link JfxView#onExposeFailure(IOException)}
+     */
     default void expose() {
         final FXMLLoader fxmlLoader = new FXMLLoader(
             getClass().getResource(layoutPath()), Application.getCurrentResourceBundle()
@@ -24,6 +39,14 @@ public sealed interface JfxView permits TopBar {
         }
     }
 
+    /**
+     * On expose failure callback. When exception will throw by
+     * {@link FXMLLoader}, this method will call.
+     *
+     * By default, does throw RuntimeException to top level.
+     *
+     * @param e exception, which was thrown.
+     */
     default void onExposeFailure(final IOException e) {
         throw new RuntimeException(e);
     }

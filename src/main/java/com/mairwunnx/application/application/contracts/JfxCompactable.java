d453836f1e6642032cf07cd05e649cd9d47abbdb
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-08 23:30:39 +0300

    JfxCompactable.java contract documented

--- src/main/java/com/mairwunnx/application/application/contracts/JfxCompactable.java
+++ src/main/java/com/mairwunnx/application/application/contracts/JfxCompactable.java
@@ -5,10 +5,28 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 
+/**
+ * Interface for all JavaFX controls can be compact, depends
+ * on stage width and height.
+ *
+ * @since 1.0.0
+ */
 public interface JfxCompactable {
+    /**
+     * @return true if compact mode for this view is enabled otherwise else.
+     */
     boolean isCompactModeEnabled();
 
+    /**
+     * Does enable compact mode for current view.
+     *
+     * @param isEnabled true to turn on compact mode otherwise false.
+     */
     void setCompactMode(final boolean isEnabled);
 
+    /**
+     * @return hash map structure with saved params before compact
+     * mode enabled.
+     */
     @NotNull HashMap<Node, Object> getCompactSavedParams();
 }

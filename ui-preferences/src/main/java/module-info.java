5930705f65d6a8b02d519f0bf4d33cea813f94d7
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-08 23:21:19 +0300

    ui-preferences new module created

---
+++ ui-preferences/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module application.ui.preferences {
+    requires org.jetbrains.annotations;
+    requires org.apache.commons.lang3;
+
+    exports com.mairwunnx.ui.preferences;
+    exports com.mairwunnx.ui.preferences.impl;
+}

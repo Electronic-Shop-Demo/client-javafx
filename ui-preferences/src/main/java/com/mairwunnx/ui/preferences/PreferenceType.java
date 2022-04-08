f3effa6e2b11a75681f8e3a73d91932ca011b0ba
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-08 23:22:10 +0300

    PreferenceType.java preference type enum class created

---
+++ ui-preferences/src/main/java/com/mairwunnx/ui/preferences/PreferenceType.java
@@ -0,0 +1,21 @@
+package com.mairwunnx.ui.preferences;
+
+/**
+ * Preference type of properties will be loaded.
+ *
+ * @since 1.0.0.
+ */
+public enum PreferenceType {
+    /**
+     * XML Properties type.
+     *
+     * @since 1.0.0.
+     */
+    XML,
+    /**
+     * Properties properties type. (Plain java properties).
+     *
+     * @since 1.0.0.
+     */
+    PROPERTIES
+}

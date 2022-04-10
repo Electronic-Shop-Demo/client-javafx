8631abda98a47d1e6235f49817116f3b07f1f72a
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-10 21:08:34 +0300

    module-info.java ui-lib module-info created

---
+++ ui-lib/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module application.ui.lib {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires org.jetbrains.annotations;
+    requires lombok;
+    requires com.google.guice;
+    requires application.ui.annotations;
+    requires application.ui.commons;
+
+    opens assets;
+    opens com.mairwunnx.ui.lib;
+    exports com.mairwunnx.ui.lib;
+}

4d3a8bcd5ecae535fd5281d092a7db6f019776ce
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-10 21:11:52 +0300

    ViewInitializationException.java view initialization exception class created

---
+++ ui-lib/src/main/java/com/mairwunnx/ui/lib/exceptions/ViewInitializationException.java
@@ -0,0 +1,7 @@
+package com.mairwunnx.ui.lib.exceptions;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class ViewInitializationException extends RuntimeException {
+}

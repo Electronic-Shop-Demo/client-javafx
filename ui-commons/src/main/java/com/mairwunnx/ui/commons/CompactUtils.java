052f93362e5dc2bc14c3db5e851661baa49a7c08
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-10 21:14:05 +0300

    CompactUtils.java compact utils class moved and documented

--- ui-commons/src/main/java/com/mairwunnx/ui/commons/CompactUtils.java
+++ ui-commons/src/main/java/com/mairwunnx/ui/commons/CompactUtils.java
@@ -1,16 +1,30 @@
-package com.mairwunnx.application.application.utils;
+package com.mairwunnx.ui.commons;
 
 import javafx.scene.Node;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Compact utils for compactable views.
+ *
+ * @since 1.0.0.
+ */
 public final class CompactUtils {
+    /**
+     * Compact view css class suffix.
+     *
+     * @since 1.0.0.
+     */
     public static final String COMPACT_CSS_CLASS_SUFFIX = "-compact";
 
-    public static void switchToCompact(
-        final boolean isCompact,
-        @NotNull final Node... node
-    ) {
+    /**
+     * Does switch to compact specified views.
+     *
+     * @param isCompact compact or not compact?
+     * @param node      target node to switch to compact.
+     * @since 1.0.0.
+     */
+    public static void switchToCompact(final boolean isCompact, @NotNull final Node... node) {
         for (final Node currentNode : node) {
             final var clazz = currentNode.getStyleClass().get(1);
 

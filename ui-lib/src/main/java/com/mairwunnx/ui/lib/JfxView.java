719ddce0ac9112ebcc0667d58461d7b4ce9ae7a0
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-10 21:11:05 +0300

    JfxView.java javafx custom view, contract created

--- ui-lib/src/main/java/com/mairwunnx/ui/lib/JfxView.java
+++ ui-lib/src/main/java/com/mairwunnx/ui/lib/JfxView.java
@@ -1,16 +1,22 @@
-package com.mairwunnx.application.application.contracts;
+package com.mairwunnx.ui.lib;
 
-import com.mairwunnx.application.application.Application;
-import com.mairwunnx.application.application.views.TopBar;
 import javafx.fxml.FXMLLoader;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 /**
  * Contract for all JavaFX custom views can be exposed.
+ *
+ * @since 1.0.0.
  */
 public sealed interface JfxView permits TopBar {
+    /**
+     * Base initialization process, calling after instantiating view class.
+     *
+     * @return {@link JfxView} class instance.
+     */
     @NotNull JfxView init();
 
     /**
@@ -21,16 +27,21 @@ public sealed interface JfxView permits TopBar {
     @NotNull String layoutPath();
 
     /**
+     * Simply supplier for receiving resource bundle for this view.
+     *
+     * @return non-nullable resource bundle.
+     */
+    @NotNull ResourceBundle resourceBundle();
+
+    /**
      * Does expose (inflate) layout for this root. Also sets controller
      * as instance to this.
-     *
+     * <p>
      * In case if {@link FXMLLoader} will throw exception that exception will
      * delegate to {@link JfxView#onExposeFailure(IOException)}
      */
     default void expose() {
-        final FXMLLoader fxmlLoader = new FXMLLoader(
-            getClass().getResource(layoutPath()), Application.getCurrentResourceBundle()
-        );
+        final var fxmlLoader = new FXMLLoader(getClass().getResource(layoutPath()), resourceBundle());
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
 
@@ -44,7 +55,7 @@ public sealed interface JfxView permits TopBar {
     /**
      * On expose failure callback. When exception will throw by
      * {@link FXMLLoader}, this method will call.
-     *
+     * <p>
      * By default, does throw RuntimeException to top level.
      *
      * @param e exception, which was thrown.

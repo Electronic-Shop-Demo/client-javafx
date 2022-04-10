bf9d51a0e48641e687227978fdf29baeb350e441
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-10 21:08:48 +0300

    module-info.java module info for ui-commons module created

---
+++ ui-commons/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module application.ui.commons {
+    requires org.jetbrains.annotations;
+    requires lombok;
+    requires javafx.graphics;
+    requires org.apache.commons.lang3;
+
+    exports com.mairwunnx.ui.commons;
+}

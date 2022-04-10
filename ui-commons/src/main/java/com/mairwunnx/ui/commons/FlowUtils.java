e58ed71b13f1206475e72d2dcc1d41d474939050
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-10 21:11:40 +0300

    FlowUtils.java flow utils class created

---
+++ ui-commons/src/main/java/com/mairwunnx/ui/commons/FlowUtils.java
@@ -0,0 +1,17 @@
+package com.mairwunnx.ui.commons;
+
+/**
+ * Programming code flow utils, some function for
+ * controlling flow.
+ *
+ * @since 1.0.0.
+ */
+public final class FlowUtils {
+    /**
+     * No-op, noop , i.e. doing nothing and return void.
+     *
+     * @since 1.0.0.
+     */
+    public static void noop() {
+    }
+}

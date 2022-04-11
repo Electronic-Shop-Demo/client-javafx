1418d9d0fa1ec926a4d6d47128556bce937a9377
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-11 14:34:45 +0300

    HomeController.java now final and extends Controller class

--- src/main/java/com/mairwunnx/application/application/controllers/HomeController.java
+++ src/main/java/com/mairwunnx/application/application/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.mairwunnx.application.application.controllers.home;
+package com.mairwunnx.application.application.controllers;
 
 import com.mairwunnx.ui.lib.TopBar;
 import com.mairwunnx.ui.navigation.Router;
@@ -6,22 +6,31 @@ import com.mairwunnx.ui.navigation.controller.RouterController;
 import com.mairwunnx.ui.navigation.types.RouterArg;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import lombok.AccessLevel;
 import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Log4j2
-public class HomeController implements RouterController {
+public final class HomeController extends Controller implements RouterController {
+    @FXML
+    @lombok.Getter(AccessLevel.PROTECTED)
+    private AnchorPane root;
+
     @FXML
     private TopBar topbar;
 
-    @FXML
-    private AnchorPane root;
-
     @Override
     public <T> void onShow(@NotNull final Router router, @Nullable final RouterArg<T> arg) {
-        root.requestFocus();
+        super.onShow(router, arg);
+
+        topbar.setBackAvailable(router.getGraph().getPages().size() > 1);
+
         root.setOnMousePressed(e -> root.requestFocus());
+        topbar.setOnLocationDialogRequested(() -> {
+            System.out.println("");
+            return "";
+        });
         topbar.setOnBackRequested(() -> log.debug("top bar back button clicked"));
         topbar.setOnSearchChanged(log::debug);
     }

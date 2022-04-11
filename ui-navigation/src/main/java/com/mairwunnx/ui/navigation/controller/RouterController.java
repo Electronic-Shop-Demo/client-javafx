d81e285f9c90dce14b40a3b29ba1c9302c385ae5
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-11 14:33:36 +0300

    RouterController.java router controller contract documented and removed default implementations

--- ui-navigation/src/main/java/com/mairwunnx/ui/navigation/controller/RouterController.java
+++ ui-navigation/src/main/java/com/mairwunnx/ui/navigation/controller/RouterController.java
@@ -5,29 +5,52 @@ import com.mairwunnx.ui.navigation.types.RouterArg;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@SuppressWarnings({"unchecked", "unused"})
+/**
+ * Router controller behavior contract.
+ * All controllers must implement this interface.
+ *
+ * @since 1.0.0.
+ */
+@SuppressWarnings("unused")
 public interface RouterController {
-    @Nullable
-    default <T> T getArgumentFromOrNull(@Nullable final RouterArg<?> arg, @Nullable final String key) {
-        if (arg != null) {
-            return arg.key().equals(key) ? (T) arg.value() : null;
-        }
+    /**
+     * @param arg argument object received from {@link RouterController#onShow(Router, RouterArg)}
+     * @param key key of argument.
+     * @param <T> generic type of argument value.
+     * @return value of argument received by specified key parameter or null.
+     * @since 1.0.0.
+     */
+    @Nullable <T> T getArgumentFromOrNull(@Nullable final RouterArg<?> arg, @Nullable final String key);
 
-        return null;
-    }
+    /**
+     * @param arg argument object received from {@link RouterController#onShow(Router, RouterArg)}
+     * @param key key of argument.
+     * @param <T> generic type of argument value.
+     * @return value of argument received by specified key parameter or throw {@link NullPointerException}.
+     * @since 1.0.0.
+     */
+    @NotNull <T> T getArgumentFrom(@Nullable final RouterArg<?> arg, @Nullable final String key);
 
-    @NotNull
-    default <T> T getArgumentFrom(@Nullable final RouterArg<?> arg, @Nullable final String key) {
-        if (arg != null) {
-            if (arg.key().equals(key)) return (T) arg.value();
-            throw new NullPointerException();
-        }
-
-        throw new NullPointerException();
-    }
-
+    /**
+     * On show controller lifecycle method.
+     * <p/>
+     * Calls after navigate calling (and after initialize from JFX called).
+     *
+     * @param router {@link Router} class instance.
+     * @param arg    {@link RouterArg} router argument class instance.
+     * @param <T>    generic of argument value type.
+     * @since 1.0.0.
+     */
     <T> void onShow(@NotNull final Router router, @Nullable final RouterArg<T> arg);
 
-    default void onExit(@NotNull final Router router) {
-    }
+    /**
+     * On exit controller lifecycle method.
+     * <p/>
+     * Calls before application shutdown when router shutdown method called.
+     *
+     * @param router {@link Router} class instance.
+     * @see Router#shutdown().
+     * @since 1.0.0.
+     */
+    void onExit(@NotNull final Router router);
 }

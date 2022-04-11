c037bca73c7251691b6364fe7dbacabbfb302009
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-11 15:06:50 +0300

    module-info.java fasterxml jackson modules removed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,9 +11,9 @@ module com.mairwunnx.application {
     requires javax.inject;
     requires com.google.guice;
 
-    requires com.fasterxml.jackson.core;
+/*    requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.databind;*/
 
     requires org.apache.commons.lang3;
     requires org.apache.logging.log4j;

131c06a8f74c41fbecca1fc211b7583d49d46594
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-17 19:59:53 +0300

    GuiceInjector.java getting stage depends on debug enabled status added

--- app/src/main/java/com/mairwunnx/application/application/di/GuiceInjector.java
+++ app/src/main/java/com/mairwunnx/application/application/di/GuiceInjector.java
@@ -1,9 +1,11 @@
 package com.mairwunnx.application.application.di;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
 import com.mairwunnx.application.PostInit;
+import com.mairwunnx.application.application.EnvironmentVariables;
 import com.mairwunnx.application.application.di.modules.AppModule;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,7 +19,15 @@ public final class GuiceInjector {
     private static Injector injector;
 
     public static void ensureModules(@NotNull final String[] args) {
-        setInjector(Guice.createInjector(Stage.PRODUCTION, new AppModule(args)));
+        setInjector(Guice.createInjector(getStage(), getModules(args)));
         getInjector().getInstance(PostInit.class).init();
+    }
+
+    private static @NotNull Stage getStage() {
+        return EnvironmentVariables.isDebugEnabled ? Stage.DEVELOPMENT : Stage.PRODUCTION;
+    }
+
+    private static @NotNull AbstractModule[] getModules(@NotNull final String[] args) {
+        return new AbstractModule[]{new AppModule(args)};
     }
 }

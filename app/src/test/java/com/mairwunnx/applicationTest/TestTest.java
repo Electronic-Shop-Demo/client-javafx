a35030485f302cc676c5a8d8a9584c751b3edc01
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-18 21:37:07 +0300

    TestTest.java test classes added

---
+++ app/src/test/java/com/mairwunnx/applicationTest/TestTest.java
@@ -0,0 +1,12 @@
+package com.mairwunnx.applicationTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+    @Test
+    public void test() {
+        System.out.println("true = " + true);
+        Assertions.assertTrue(1 == 5);
+    }
+}
---
+++ ui-lib/src/test/java/com/mairwunnx/libtests/UiLibTest.java
@@ -0,0 +1,12 @@
+package com.mairwunnx.libtests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UiLibTest {
+    @Test
+    public void test() {
+        System.out.println("false = " + true);
+        Assertions.assertTrue(1 == 4);
+    }
+}

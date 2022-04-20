b3ad9c379dde66a8f7356bcddb38d5970a33cd5a
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-20 12:39:44 +0300

    ZygoteInit.java checking for debug enabled added

--- app/src/main/java/com/mairwunnx/application/ZygoteInit.java
+++ app/src/main/java/com/mairwunnx/application/ZygoteInit.java
@@ -1,10 +1,15 @@
 package com.mairwunnx.application;
 
+import com.mairwunnx.application.application.EnvironmentVariables;
 import com.mairwunnx.application.application.di.GuiceInjector;
 import org.jetbrains.annotations.Nullable;
 
 public final class ZygoteInit {
     public static void main(@Nullable final String[] args) {
+        if (EnvironmentVariables.isDebugEnabled) {
+            System.setProperty("log4j2.configurationFile", "log4j2-debug.xml");
+        }
+
         GuiceInjector.ensureModules(args == null ? new String[]{} : args);
     }
 }
